7bf1b438cd1e3598c7ad6b98032aa1f58842e8b7
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-10-13 22:31:28 -0500

    request works!

--- src/projects/project2/webserver.py
+++ src/projects/project2/webserver.py
@@ -1,5 +1,6 @@
 """Python Web server implementation"""
 from socket import socket, AF_INET, SOCK_STREAM
+import logging
 
 
 ADDRESS = "127.0.0.2"  # Local client is going to be 127.0.0.1
@@ -11,7 +12,19 @@ def main():
     """Main loop"""
     with socket(AF_INET, SOCK_STREAM) as server_sock:
         server_sock.bind((ADDRESS, PORT))
+        server_sock.listen()
+        print("The server is ready to receive")
+        while True:
+            connectionSocket, addr = server_sock.accept()
+            request = connectionSocket.recv(1024).decode()
+            print("test?",request)
 
+            connectionSocket.send("Test".encode())
+            connectionSocket.close()
+            # if request != "/alice30.txt":
+            #     connectionSocket.send()
+
+        
 
 if __name__ == "__main__":
     main()
