c7993b3f8fa532169fd2c4b61757a8112e256497
Temuulen Erdenebulgan <erdete01@luther.edu>
2020-10-17 17:59:32 -0500

    added

--- src/projects/project2/webserver.py
+++ src/projects/project2/webserver.py
@@ -2,36 +2,62 @@
 from socket import socket, AF_INET, SOCK_STREAM
 import logging
 import argparse
+import time
+import datetime
+import json
 
 
 ADDRESS = "127.0.0.2"  # Local client is going to be 127.0.0.1
 PORT = 4300  # Open http://127.0.0.2:4300 in a browser
 LOGFILE = "webserver.log"
 
-def response():
-    # string = open('alice30.txt').read()
-    # myList = []
-    # for word in string.split():
-    #     myList.append(word)
-    # print(len(myList))
-    return 14858
+def format(message: str) -> bytes:
+    """Convert (encode) the message to bytes"""
+    return message.encode()
+
+def contentLength():
+    string = open('alice30.txt').read()
+    return str(len(string))
+
+def logFile(myDictionary):
+    f = open("webserver.log", "a")
+    f.write(myDictionary["Time"] + " | " + 
+        myDictionary["file"] + " | " + myDictionary["IP"] 
+        + " | " + myDictionary["browser"] + "\n")
+    f.close()
 
 def server_loop():
     with socket(AF_INET, SOCK_STREAM) as server_sock:
         server_sock.bind((ADDRESS, PORT))
         server_sock.listen()
+        myDict = {}
         print("The server is ready to receive")
         while True:
             connectionSocket, addr = server_sock.accept()
             request = connectionSocket.recv(1024).decode()
-            print(request)
             if request:
-
-                ip = socket.gethostbyname(http://127.0.0.2:4300)
-            connectionSocket.send("Test".encode(), ADDRESS)
-        connectionSocket.close()
-            # if request != "/alice30.txt":
-            #     connectionSocket.send()
+                # print(request)
+                myDict['HTTP'] = request.split( )[2]
+                myDict['server'] = "CS430-Temuulen Erdenebulgan"
+                myDict['date'] = time.asctime( time.localtime(time.time()) )
+                myDict['file'] = request.split( )[1]
+                lines = request.split( )
+                browser = lines.index('User-Agent:') + 1
+                myDict['browser'] = request.split( )[browser]
+                myDict["Time"] = str(datetime.datetime.now())
+                ip = lines.index('Host:') + 1
+                
+                print(request.split(":"))
+                myDict["IP"] = request.split(":")[ip]
+                myDict["length"] = contentLength()
+                myDict["modified"] = "Friday, August 29, 2018 11:00 AM"
+                myDict["content-type"] = "text/plain; charset=utf-8"
+                logFile(myDict)
+                # print("hello?",myDict)
+                # response = myDict['HTTP'] + "200 OK" + "\n" + "Content-Length: " + myDict["length"] + "\n" + "Content-Type: " + myDict["content-type"] +"\n" + "Date: " + myDict['date'] + '\n' + "Last-Modified: " + myDict["modified"] + "\n" + "Server: " + myDict["server"]
+                
+                connectionSocket.sendto(format("hi"), addr)
+                
 
 def main():
     """Main loop"""
@@ -48,7 +74,6 @@ def main():
     else:
         logger.setLevel(logging.WARNING)
     logging.basicConfig(format="%(levelname)s: %(message)s", level=logger.level)
-    # print(response())
     server_loop()
         
 
